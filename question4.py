112030af353c96533bc0d83602add0e45f305afb
Aju <bectorarjun@gmail.com>
2025-12-31 17:54:41 +0000

    Answered Question 4
    brute force approach to answer question 4

--- question4.py
+++ question4.py
@@ -1,1 +1,15 @@
 # Solution for Question 4 - Find the largest palindrome made from the product of two 3-digit numbers
+
+def isPalindrome(n):
+    return str(n) == str(n)[::-1]
+
+largest = 0
+
+# brute force and check each product
+for i in range(100, 1000):
+    for j in range(i, 1000):
+        product = i * j
+        if isPalindrome(product) and product > largest:
+            largest = product
+
+print("Question 4: " + str(largest))
